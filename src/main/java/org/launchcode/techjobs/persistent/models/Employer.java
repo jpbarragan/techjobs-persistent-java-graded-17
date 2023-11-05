683f31fb9ff4da3e74bb5a8f7af9c90acf1e2954
jpbarragan <jpbarragann@gmail.com>
2023-11-05 17:29:19 -0500

    Finished Part 3 of GA#4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,11 @@ public class Employer extends AbstractEntity {
     @NotNull
     @Size (min = 3, max = 50)
     private String location;
+
+    //@OneToMany(mappedBy = "employer")
+    @OneToMany
+    @JoinColumn(name = "employer_id") // The name parameter indicates that Employer will have a foreign key column named employer_id referring to the id in the class Job
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
@@ -19,6 +29,10 @@ public class Employer extends AbstractEntity {
 
     public String getLocation() {
         return location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
     public void setLocation(String location) {
